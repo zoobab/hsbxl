3f028866b3ac535e86429c35ff52690af21c5212
Friedger Müffke <friedger@gmail.com>
2015-10-27 23:44:29 +0100

    fix modification date

--- welcome/js/projects.js
+++ welcome/js/projects.js
@@ -1,7 +1,7 @@
 
 function ProjectsCtrl($scope, $window, $rootElement, $http, $filter) {
     var now = new Date();
-    var nowMinusSixMonth = new Date(now.setMonth($scope.dt.getMonth() - 6)).toISOString().slice(0, 10);
+    var nowMinusSixMonth = new Date(now.setMonth(now.getMonth() - 6)).toISOString().slice(0, 10);
     $http({method: 'GET', url: 'https://hackerspace.be/Special:Ask/-5B-5BCategory:Project-5D-5D-20-5B-5Barchived::false-5D-5D-20-5B-5BModification-20-date::>'+nowMinusSixMonths+'-5D-5D/-3FImage-23-2D/-3FDescription-23-2D/-3FStart-20date-23-2D/-3FPeople-23-2D/-3FArchived-23-2D/format%3Djson/prettyprint%3Dtrue/offset%3D0'}).
               success(function(data, status, headers, config) {
                   console.log(data.results);
